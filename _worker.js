095f6edb15fd264e1732f5630e275963a8e148a2
Lrsu <105852378+lrsu@users.noreply.github.com>
2024-04-15 21:50:21 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -6,6 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '607be565-a38c-457f-9ac9-f867212296b7';
 
+// 移动优化
 const proxyIPs = ["172.67.149.164"];//['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
